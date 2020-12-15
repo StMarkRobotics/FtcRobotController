039a0f258c0a611a18db07e99b734b569dfe812b
Stefan Allen <stmarkrobotics@gmail.com>
2020-12-14 21:03:57 -0500

    Testing Android Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_SourceTree.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_SourceTree.java
@@ -51,7 +51,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
+@TeleOp(name="Pushbot: Testing", group="Pushbot")
 @Disabled
 public class Test_SourceTree extends OpMode{
 
