d664da6b77ff7846f9fa6b4e1ba5c2a79771c97d
Katherine Berger <2021kberger@stmark-school.org>
2021-01-18 20:26:55 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Park.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Park.java
@@ -48,7 +48,7 @@ public class Park extends LinearOpMode {
         robot.RearLeft.setPower(FORWARD_SPEED);
         robot.RearRight.setPower(FORWARD_SPEED);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 4.2)) {
+        while (opModeIsActive() && (runtime.seconds() < 4.1)) {
             telemetry.addData("Path", "Part One: Moving forwards to get off wall.", runtime.seconds());
             telemetry.update();
         }
