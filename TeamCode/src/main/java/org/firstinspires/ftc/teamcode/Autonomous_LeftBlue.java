a40cd6e152a2e7c9dd9b20e861cae23f212644ff
Katherine Berger <2021kberger@stmark-school.org>
2021-01-18 19:54:40 -0500

    updated times

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
@@ -45,7 +45,7 @@ public class Autonomous_LeftBlue extends LinearOpMode {
         robot.RearLeft.setPower(FORWARD_SPEED);
         robot.RearRight.setPower(FORWARD_SPEED);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 2.7)) {
+        while (opModeIsActive() && (runtime.seconds() < 2.9)) {
             telemetry.addData("Path", "Part One: Moving forwards to get off wall.", runtime.seconds());
             telemetry.update();
         }
