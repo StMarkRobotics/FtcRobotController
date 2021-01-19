6e53054e64c8f3bef28406d43d192455c8f444fe
Katherine Berger <2021kberger@stmark-school.org>
2021-01-18 20:32:48 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * This file uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode.
+ * The code is structured as a LinearOpMode
  */
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous_LeftBlue", group="Pushbot")
