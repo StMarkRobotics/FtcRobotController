7887c0ac82ca15e8cb9f768aad6153fefeacadea
Katherine Berger <2021kberger@stmark-school.org>
2021-01-18 20:04:16 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftBlue.java
@@ -56,7 +56,7 @@ public class Autonomous_LeftBlue extends LinearOpMode {
         robot.RearLeft.setPower(-FORWARD_SPEED);
         robot.RearRight.setPower(FORWARD_SPEED);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < .5)) {
+        while (opModeIsActive() && (runtime.seconds() < .7)) {
             telemetry.addData("Path", "Part Two: Strafing to starter stack.", runtime.seconds());
             telemetry.update();
         }
@@ -83,7 +83,7 @@ public class Autonomous_LeftBlue extends LinearOpMode {
         robot.RearLeft.setPower(FORWARD_SPEED);
         robot.RearRight.setPower(-FORWARD_SPEED);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < .5)) {
+        while (opModeIsActive() && (runtime.seconds() < .7)) {
             telemetry.addData("Path", "Part Four: Strafing back to wobble thing. ", runtime.seconds());
             telemetry.update();
         }
