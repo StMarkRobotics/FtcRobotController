57d86328f9ca4b310dafe941ce7bf68c92c3c780
Katherine Berger <2021kberger@stmark-school.org>
2021-02-07 15:46:48 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_LeftRed.java
@@ -98,7 +98,7 @@ public class Autonomous_LeftRed extends LinearOpMode {
             robot.RearLeft.setPower(TURN_SPEED);
             robot.RearRight.setPower(-TURN_SPEED);
             runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() < .8)) {
+            while (opModeIsActive() && (runtime.seconds() < .9)) {
                 telemetry.addData("Path", "Part Five: Starter Stack 0- Turning towards target zone A. ", runtime.seconds());
                 telemetry.update();
             }
