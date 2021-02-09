a2e557245fe61e67950c5bf2eacc897b0d00b5d0
Abena LAPTOP-49L0T2TP\junev <2021apoku@stmark-school.org>
2021-02-08 20:27:31 -0500

    changed name from "driver code 2" to "driver code"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_IterativeAbenapt2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_IterativeAbenapt2.java
@@ -52,7 +52,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Driver Code 2", group="Pushbot")
+@TeleOp(name="Driver Code", group="Pushbot")
 //@Disabled
 public class PushbotTeleopTank_IterativeAbenapt2 extends OpMode{
 
